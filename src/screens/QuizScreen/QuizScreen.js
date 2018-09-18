165ac283cf8de418537ff3f95790b6bf288bd98b
NajamShehzad <najambutt195@gmail.com>
2018-09-18 07:47:19 +0500

    Adding Manifest.Json

--- src/screens/QuizScreen/QuizScreen.js
+++ src/screens/QuizScreen/QuizScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import Timer from './TImer';
 
-
-
-
+let time1;
+let quizTime;
+let allQuestions;
 class QuizScreen extends Component {
     constructor() {
         super()
@@ -10,11 +12,14 @@ class QuizScreen extends Component {
             quizNo: 0,
             givinAnswer: '',
             allAnswers: [],
-            quizCode: '123456',
-            startQuiz: false
+            quizCode: '',
+            Quiz: false,
+            time: ''
         }
         this.getValue = this.getValue.bind(this);
         this.submitAnswer = this.submitAnswer.bind(this);
+        this.start = this.start.bind(this);
+        this.backup = this.backup.bind(this);
     }
 
 
@@ -23,7 +28,8 @@ class QuizScreen extends Component {
         this.setState({ givinAnswer });
     }
 
-    submitAnswer() {
+    submitAnswer(e) {
+        e.preventDefault();
         const { startQuiz } = this.props;
         const { quizNo, givinAnswer, allAnswers } = this.state;
         const correctAnswer = startQuiz.questions[quizNo].correctAnswer;
@@ -33,6 +39,7 @@ class QuizScreen extends Component {
             quizNo
         }
         allAnswers.push(answer);
+        allQuestions = allAnswers
 
         if (quizNo + 1 < startQuiz.questions.length) {
             this.setState({ allAnswers, givinAnswer: '', quizNo: quizNo + 1 })
@@ -44,45 +51,90 @@ class QuizScreen extends Component {
         }
 
     }
+    form(e) {
+        e.preverntDefault();
+    }
+    backup(){
+        this.props.showMarks(allQuestions);
+    }
     renderQuiz() {
         const { startQuiz } = this.props;
         const { quizNo } = this.state;
         const quizQuestion = startQuiz.questions[quizNo];
         return (
             <div>
+                <Timer timerOut={this.backup} />
                 <div>
                     {quizQuestion.question}
                 </div>
-                <ul>
-                    {quizQuestion.options.map((option, index) => {
-                        return (
-                            <li key={option}>
-                                <div className="radio">
-                                    <label >
-                                        <input onClick={this.getValue} value={index} name={quizQuestion.question} type="radio" />
-                                        {option}
-                                    </label>
-                                </div>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <div>
-                    <button onClick={this.submitAnswer}>
-                        Next
+                <form onSubmit={this.submitAnswer}>
+                    <ul>
+                        {quizQuestion.options.map((option, index) => {
+                            return (
+                                <li key={option}>
+                                    <div className="radio">
+                                        <label >
+                                            <input required onClick={this.getValue} value={index} name={quizQuestion.question} type="radio" />
+                                            {option}
+                                        </label>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <div>
+                        <button>
+                            Next
                     </button>
-                </div>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+    start(e) {
+        e.preventDefault();
+        const { quizCode } = this.state;
+        if (quizCode === "123456") {
+            this.setState({ Quiz: true })
+            time1 = Date.now();
+            console.log(time1);
+            quizTime= time1 + 200000;
+            console.log(quizTime);
+            
+            
+        }
+
+       
+    }
+
+    passowrdField() {
+        return (
+            <div>
+                <form onSubmit={this.start.bind(this)}>
+                    <input
+                        value={this.state.quizCode}
+                        onChange={(e) => this.setState({ quizCode: e.target.value })}
+                        placeholder="Please Enter The Quiz Password And Hit Enter"
+                        style={{ width: "40%" }}
+                    />
+                </form>
             </div>
         )
     }
 
     render() {
         const { startQuiz } = this.props;
-        const { quizNo } = this.state;
+        const { quizNo, Quiz } = this.state;
         const quizQuestion = startQuiz.questions[quizNo];
         return (
             <div>
-                {this.renderQuiz()}
+                <div>
+                   
+                </div>
+                {Quiz ?
+                    this.renderQuiz()
+                    : this.passowrdField()
+                }
             </div>
         )
     }
